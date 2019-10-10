4cc3a3b7d31edaf84757085427b02732b68f0b76
Lucas Ataides <lcsataides@gmail.com>
2019-10-10 10:52:48 -0300

    added value for current ip and current date on input

--- main.js
+++ main.js
@@ -10,15 +10,19 @@ export default () => {
 		var currentDate = new Date();
 		var date = currentDate.getDate();
 		var month = currentDate.getMonth(); //Be careful! January is 0 not 1
-		var year = currentDate.getFullYear();
+    var year = currentDate.getFullYear();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
 
-		var ip = "";
-		var dateString = date + "-" +(month + 1) + "-" + year;
+    var ip = "";
+		var dateString = date + "-" +(month + 1) + "-" + year + " " + hours + ":" + minutes + ":" + seconds;
 
 		ip = geoplugin_request();
 		document.querySelector(".date-hidden").value = dateString;
     document.querySelector(".ip-hidden").value = ip;
 
+    console.log(currentDate);
     console.log(dateString);
     console.log(ip);
   }
